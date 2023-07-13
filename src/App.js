2c6b430dbedb1805c421efcd13ed4441b36b60da
Prerna20000 <122144396+Prerna20000@users.noreply.github.com>
2023-07-13 00:41:30 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+const api = {
+    key : "2ed3c4875b237b4f224f5fc884ec8004",
+    base: "https://home.openweathermap.org/api_keys"
+}
+
+function App(){
+    return(
+        < div className="App">
+        </div>
+    )
+}
+
+ export default App;
